20f628e799806edbc66303f06df60d071816cc39
Zero <yen.lo@twbeex.com>
2023-02-26 17:07:36 +0800

    first commit

---
+++ src/pages/openocean.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Web3 from 'web3'
+import { OpenoceanApiSdk } from '@openocean.finance/api';
+
+// const openoceanApiSdk = new OpenoceanApiSdk()
+// const { api, swapSdk, config } = openoceanApiSdk
+
+// api.getTokenList({
+//     chain: 'BSC',
+// }).then((data) => {
+//     console.log(data)
+// }).catch((error) => {
+//     return
+// });
+
+function openocean() {
+  return (
+    <div>openocean</div>
+  )
+}
+
+export default openocean;
